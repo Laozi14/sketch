277fae320a2b2fefc9fbbb46987c1b456f50a6de
Laozi14 <rohit22rm@gmail.com>
2024-11-06 18:19:30 +0530

    Added Button to dynamically create new sketch pad from Users input of number of grids per side.

--- sketch.js
+++ sketch.js
@@ -1,16 +1,37 @@
 const container = document.querySelector(".container");
 
-const totalGrids = 16*16;
-//Use for loop for creating multiple grids
-for(let i =0; i < totalGrids; i++){
-    const grid = document.createElement("div");
-    console.log(grid);
-    grid.classList.add("grid");
-//Add hovered class to change color of the grind when hovered    
-    grid.addEventListener('mouseenter',()=>{
-        grid.classList.add('hovered');
-    })
-    container.appendChild(grid);
+const button = document.getElementById("btn");
+
+
+function populateScreen(gridPerSide){
+
+    clearGrid();
+    let sizePerSide = 960 / gridPerSide;
+    for(let i =0; i < gridPerSide * gridPerSide; i++){
+        const grid = document.createElement("div");
+        grid.classList.add("grid");
+        grid.style.height =`${sizePerSide}px`;
+        grid.style.width = `${sizePerSide}px`;
+//Add hovered class to change color of the grid when hovered    
+        grid.addEventListener('mouseenter',()=>{
+            grid.classList.add('hovered');
+        })
+        container.appendChild(grid);
+    }
 }
+function clearGrid(){
+    while(container.firstChild){
+        container.removeChild(container.firstChild);
+    }
+}
+populateScreen(16);
 
-
+button.addEventListener('click', ()=>{
+    let gridPerSide = parseInt(prompt("Number of Squares per side?"));
+    if(!isNaN(gridPerSide) && gridPerSide > 0 && gridPerSide <= 100){
+        populateScreen(gridPerSide);
+    } else{
+        alert(`Please enter valid number between 1 and 100`);
+    }
+    
+})
